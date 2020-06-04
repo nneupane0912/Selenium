824f737117458163ed3b981f08dd4f18e7dbf585
neupa <neupa@LAPTOP-64B6IR7R>
2020-06-04 18:53:44 -0500

    NewProject

---
+++ NeupaneN/src/com/Homework/Java/NewProject.java
@@ -0,0 +1,12 @@
+package com.Homework.Java;
+
+public class NewProject 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        System.out.println("hello");
+	}
+
+}
